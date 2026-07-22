e2e8bf4fd098b19f84d1496e71b37b9b3b49bb5c
Jerry0w0 <jerry@example.com>
2026-07-22 21:43:34 +0800

    feat: server front-end

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ app = FastAPI(title="GitHub 项目体检 API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
